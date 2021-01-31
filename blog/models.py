f7dcefd055f9069e3f9769e14fb01b24a6b0793f
ohwellmylove <maowens66@gmail.com>
2021-01-31 14:50:54 -0500

    Added the ability to upload images with posts

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,8 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='blog_images')
+
 
     def __str__(self):
         return self.title
